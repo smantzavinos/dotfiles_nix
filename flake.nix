423af271e24405d3b1c860a2262f1539beaee922
Spiros Mantzavinos <smantzavinos@gmail.com>
2023-12-31 22:31:10 -0500

    Basic working config.
    emacs mostly working.
    vim not working well.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+    description = "My Home Manager Flake";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+        home-manager = {
+            url = "github:nix-community/home-manager/release-23.11";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = {nixpkgs, home-manager, ...}: {
+        # For `nix run .` later
+        defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+
+        homeConfigurations = {
+            "spiros" = home-manager.lib.homeManagerConfiguration {
+                # Note: I am sure this could be done better with flake-utils or something
+                pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+                modules = [ ./home.nix ];
+            };
+        };
+    };
+}
+
---
+++ home.nix
@@ -0,0 +1,41 @@
+{pkgs, ...}: {
+    home.username = "spiros";
+    home.homeDirectory = "/home/spiros";
+    home.stateVersion = "23.11"; # To figure this out you can comment out the line and see what version it expected.
+    programs.home-manager.enable = true;
+
+    home.packages = [
+      pkgs.cowsay
+      pkgs.gh
+      pkgs.jetbrains-mono
+      pkgs.emacs-all-the-icons-fonts
+      # pkgs.awesome-terminal-fonts
+      pkgs.material-icons
+      pkgs.weather-icons
+      pkgs.fd
+    ];
+
+    programs.git = {
+      enable = true;
+      userName = "Spiros Mantzavinos";
+      userEmail = "smantzavinos@gmail.com";
+    };
+
+    programs.neovim = {
+      enable = true;
+      vimAlias = true;
+      # vim config not working correctly.
+      # need a way to manage plugins
+      extraConfig = ''
+        source /home/spiros/vimfiles/_vimrc
+      '';
+    };
+
+    programs.emacs = {
+      enable = true;
+      package = pkgs.emacs;
+      extraConfig = ''
+        (load-file "/home/spiros/dotfiles/.emacs.d/init_local.el")
+      '';
+    };
+}
