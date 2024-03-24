79a3ea958c752cba756474ca845aad47eca8e02d
Spiros Mantzavinos <smantzavinos@gmail.com>
2024-03-24 13:01:33 -0400

    format change

--- systems/lenovo_x1_extreme.nix
+++ systems/lenovo_x1_extreme.nix
@@ -10,7 +10,7 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"]; # or
+  services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
     modesetting.enable = true;
